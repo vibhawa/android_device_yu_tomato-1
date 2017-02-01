f84ef72801b7f91959bfeb8c23a519c9fb212c60
C0de_Fre@k <ayushwalia999@gmail.com>
2017-02-01 16:11:29 +0530

    Fixed some shits

--- device.mk
+++ device.mk
@@ -26,19 +26,5 @@ include $(LOCAL_PATH)/product/*.mk
 
 $(call inherit-product-if-exists, vendor/yu/tomato/tomato-vendor.mk)
 
-PRODUCT_PACKAGES += \
-    IMSEnabler
-
-PRODUCT_PROPERTY_OVERRIDES += \
-service.qti.ims.enabled=1 \
-persist.volte_enalbed_by_hw=1 \
-net.lte.volte_call_capable=true \
-persist.ims.volte=true \
-persist.radio.ROTATION_ENABLE=1 \
-persist.radio.VT_ENABLE=1 \
-persist.radio.VT_HYBRID_ENABLE=1 \
-persist.radio.VT_USE_MDM_TIME=0 \
-persist.radio.calls.on.ims=true \
-DEVICE_PROVISIONED=1
-
+# AUDIO
 AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
