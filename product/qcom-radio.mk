d3f47a634a9e6f57b219e72fe3173b1f6f9ef504
C0de_Fre@k <ayushwalia999@gmail.com>
2017-02-02 10:58:35 +0530

    IMS Enabler

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -4,8 +4,8 @@ persist.data.target=dpm1
 persist.radio.multisim.config=dsds \
 ro.config.always_show_roaming=true \
 rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
- ril.ecclist=000,08,100,101,102,110,112,118,119,120,122,911,999 \
- ro.telephony.default_network=9,9 
+ril.ecclist=000,08,100,101,102,110,112,118,119,120,122,911,999 \
+ro.telephony.default_network=9,9 
 
 # RIL
 ifeq ($(QCPATH),)
